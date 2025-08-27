fa95400facbc182126261abf8aebf0d6230630d9
unknown <sameer.up78@gmail.com>
2025-08-27 14:39:30 +0530

    new

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+  backend "azurerm" {
+      subscription_id = "b02b567a-e4cb-4c73-975e-4664c82c8fc3"
+
+    resource_group_name  = "tej-rg"
+    storage_account_name = "ranastoragee"
+    container_name       = "jaffa"
+    key                  = "terraform.tfstate"
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b02b567a-e4cb-4c73-975e-4664c82c8fc3"
+}
+
+resource "azurerm_resource_group" "jaadugar" {
+  name     = "jaadu-rg"
+  location = "East US"
+}
+
+resource "azurerm_resource_group" "waadugar" {
+  name     = "waadu-rg"
+  location = "East US"
+}
